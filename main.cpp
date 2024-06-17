f694b67f3d5ec71b2e63ba1921eea0bc0feadd54
terminological-inexactitude <georgeboy389@gmail.com>
2024-06-17 23:10:03 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <cstdio>
 
 int main() {
-    printf("%s\n", "asdf");
+    printf("%s\n", ":stuck_out_tongue_closed_eyes::stuck_out_tongue_winking_eye::stuck_out_tongue_closed_eyes::stuck_out_tongue_winking_eye::stuck_out_tongue_winking_eye::stuck_out_tongue_winking_eye::stuck_out_tongue:\n:confetti_ball::tada::confetti_ball::tada::confetti_ball::tada::confetti_ball:\n:banana::banana::banana::banana::banana::banana::banana:\n:exclamation::grey_exclamation:BANANA:grey_exclamation::exclamation: \n:banana::banana::banana::banana::banana::banana::banana:\n:confetti_ball::tada::confetti_ball::tada::confetti_ball::tada::confetti_ball:\n:stuck_out_tongue_closed_eyes::stuck_out_tongue_winking_eye::stuck_out_tongue_closed_eyes::stuck_out_tongue_winking_eye::stuck_out_tongue_winking_eye::stuck_out_tongue_winking_eye::stuck_out_tongue:");
     return 0;
 }
