59ee6f626b0799f27b84b78358fb897384582c9e
terminological-inexactitude <georgeboy389@gmail.com>
2024-06-17 23:18:56 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 int main() {
     printf("%s\n", ":stuck_out_tongue_closed_eyes::stuck_out_tongue_closed_eyes::stuck_out_tongue_closed_eyes::stuck_out_tongue_winking_eye::stuck_out_tongue_closed_eyes::stuck_out_tongue_winking_eye::stuck_out_tongue_winking_eye::stuck_out_tongue_winking_eye::stuck_out_tongue:\n:confetti_ball::tada::confetti_ball::tada::confetti_ball::tada::confetti_ball:\n:banana::banana::banana::banana::banana::banana::banana:\n:exclamation::grey_exclamation:BANANA:grey_exclamation::exclamation: \n:banana::banana::banana::banana::banana::banana::banana:\n:confetti_ball::tada::confetti_ball::tada::confetti_ball::tada::confetti_ball:\n:stuck_out_tongue_closed_eyes::stuck_out_tongue_winking_eye::stuck_out_tongue_closed_eyes::stuck_out_tongue_winking_eye::stuck_out_tongue_winking_eye::stuck_out_tongue_winking_eye::stuck_out_tongue:");
     
-    printf("%s\n", 
+    printf("%s\n", "");
     
     printf("%s\n", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
     
