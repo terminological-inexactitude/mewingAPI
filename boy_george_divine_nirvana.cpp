5ecee8c9f4cd310f8c2ebf7e3e9bbb55ca0f4911
terminological-inexactitude <georgeboy389@gmail.com>
2024-06-17 23:37:23 +0200

    Update boy_george_divine_nirvana.cpp

--- boy_george_divine_nirvana.cpp
+++ boy_george_divine_nirvana.cpp
@@ -1,1 +1,14 @@
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+  
+int main() {
+  srand(time(NULL));
+  int p = rand() % 5 + 1;
 
+  const char* s;
+
+  printf("%s\n", "https://media.discordapp.net/attachments/1216672160286900244/1251094329188159529/GP-0fqzaoAAVMQ8.png?ex=66714864&is=666ff6e4&hm=e64bee83cf2beb6f5b21def606e192f92364e9043130c77175129dfa1c28ca0f&=&format=webp&quality=lossless&width=716&height=671");
+ 
+  return 0;
+}
