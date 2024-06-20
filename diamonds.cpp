b7f81d8be81000857ab3239c12411dcc9b98cf1c
terminological-inexactitude <georgeboy389@gmail.com>
2024-06-20 14:46:54 +0200

    Update diamonds.cpp

--- diamonds.cpp
+++ diamonds.cpp
@@ -4,17 +4,17 @@ int main()
 {
    printf("%c", 0x00);
 
-for(unsigned short y = 0; y <= 6; y++){
-   for(int i = 1; i < 10; i++){
-      for(int p = 10; p > i; p--){
-         printf("%c", ' ');
+   for(unsigned short y = 0; y <= 6; y++){
+      for(int i = 1; i < 10; i++){
+         for(int p = 10; p > i; p--){
+            printf("%c", ' ');
+         }
+   
+         for(int k = 0; k < i; k++){
+            printf("%c%c", ' ', '+');
+         }
+         printf("\n");
       }
-
-      for(int k = 0; k < i; k++){
-         printf("%c%c", ' ', '+');
-      }
-      printf("\n");
-   }
 
    for(int i = 0; i < 9; i++){
       for(int p = 0; p < i; p++){
@@ -23,10 +23,11 @@ for(unsigned short y = 0; y <= 6; y++){
 
        for(int p = 10; p > i; p--){
           printf("%c%c", ' ', '+');
-       }
+      }
+      
       printf("\n");
-  }
-}
+      }
+   }
 
-  return 0; 
+   return 0; 
 }
