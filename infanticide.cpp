de46d035094353a6b6cc4bb50aa7c8e7832604ba
terminological-inexactitude <georgeboy389@gmail.com>
2024-06-26 16:09:28 +0200

    Update infanticide.cpp

--- infanticide.cpp
+++ infanticide.cpp
@@ -26,7 +26,7 @@ int main ()
     else if(input == "restart" || input == "reset" || input == "stop"){
 		printf("Resetting board! Start a new game :)"); 
 		char clear[] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '0', '\0'};
-		write_var(clear)
+		write_var(clear);
         return 1;
     }
     else{
