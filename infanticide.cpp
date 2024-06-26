e810e0f8f10bc90f28fed13b4f05d733cb187ab9
terminological-inexactitude <georgeboy389@gmail.com>
2024-06-26 16:34:53 +0200

    Update infanticide.cpp

--- infanticide.cpp
+++ infanticide.cpp
@@ -52,13 +52,11 @@ int main ()
 
     // PRINT BOARD TO DISPLAY
     printf("\n");
-    printf("```");
     printf(" %c | %c | %c\n", board[0][0], board[0][1], board[0][2]);
     printf("---+---+---\n");
     printf(" %c | %c | %c\n", board[1][0], board[1][1], board[1][2]);
     printf("---+---+---\n");
     printf(" %c | %c | %c\n", board[2][0], board[2][1], board[2][2]);
-    printf("```");
     printf("\n");
 
     // READ BOARD
