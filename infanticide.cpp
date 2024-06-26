eff3339000d9c6afade5cd335b52b9d2e4a2a0b7
terminological-inexactitude <georgeboy389@gmail.com>
2024-06-26 16:32:09 +0200

    Update infanticide.cpp

--- infanticide.cpp
+++ infanticide.cpp
@@ -25,7 +25,7 @@ int main ()
     } 
     else if(input == "restart" || input == "reset" || input == "stop"){
 		printf("Resetting board! Start a new game :)"); 
-		char clear[] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '0', '\0'}; 
+		char clear[11] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '0', '\0'}; 
 		write_var(clear);
         return 1;
     }
@@ -88,4 +88,6 @@ void write_var(char array[]){
     })";
 	file << jsonString << std::endl;
 	file.close();
-}}
+}
+
+}
