d709135674a230d8336842eeb8bdb51044babf57
terminological-inexactitude <georgeboy389@gmail.com>
2024-06-27 10:50:40 +0200

    Update infanticide.cpp

--- infanticide.cpp
+++ infanticide.cpp
@@ -12,10 +12,12 @@ void clear_board();
 
 int main ()
 {
-    char board[3][3];
-    char oneDArray[11]; 
-    oneDArray[10] = '\0'; 
+    char board[3][3];		// playing board
+    char oneDArray[11];		// playing board in 1D format to write to JSON
+    oneDArray[10] = '\0'; 	// null terminator 
     int row, col;
+	
+	printf("\nasdfasf\n");
 
     // READ USER INPUT
     if (input[0] >= '1' && input[0] <= '3' && input[2] >= '1' && input[2] <= '3'){
@@ -47,7 +49,7 @@ int main ()
 	update_board(row, col, 'X', board);
 	
 	// BOT MOVE
-	update_board(2, 1, 'O', board); 
+	update_board(2, 1, 'O', board); // hardcoded move for now
 
     // PRINT BOARD TO DISPLAY
     printf("\n");
@@ -59,6 +61,15 @@ int main ()
     printf(" %c | %c | %c\n", board[2][0], board[2][1], board[2][2]);
     printf("```");
     printf("\n");
+
+	// CHECK WINNER 
+	 if (check_winner(board, 'X')) {
+		printf("\nYOU WIN!\n");
+	 }
+	
+	if (check_winner(board, 'O')) {
+		printf("\nYOU LOSE!\n");
+	}
 
     // READ BOARD
     for (int i = 0; i < 3; ++i) {
@@ -78,6 +89,24 @@ int main ()
     write_var(oneDArray);
 	
     return 0;
+}
+
+bool check_winner(char board[3][3], char player) {
+    // Check rows and columns
+    for (int i = 0; i < 3; ++i) {
+        if ((board[i][0] == player && board[i][1] == player && board[i][2] == player) ||
+            (board[0][i] == player && board[1][i] == player && board[2][i] == player)) {
+            return true;
+        }
+    }
+
+    // Check diagonals
+    if ((board[0][0] == player && board[1][1] == player && board[2][2] == player) ||
+        (board[0][2] == player && board[1][1] == player && board[2][0] == player)) {
+        return true;
+    }
+
+    return false;
 }
 
 void update_board(int row, int col, char mark, char board[3][3]) {
