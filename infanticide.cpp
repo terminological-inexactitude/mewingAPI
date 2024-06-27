b98dc4997d35bf451c1a6d3117e1fb680b928bb7
terminological-inexactitude <georgeboy389@gmail.com>
2024-06-27 11:51:42 +0200

    Update infanticide.cpp

--- infanticide.cpp
+++ infanticide.cpp
@@ -29,7 +29,7 @@ int main ()
         return 1;
     }
     else{
-        printf("Invalid input format:bangbang:\nUse .t tictactoe 1 3 for example\n");
+        printf("Invalid input format:bangbang:\nUse `.t tictactoe 1 3` for example\n");
         return 1;
     }
 
