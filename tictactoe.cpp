9b5b56fe40484c8f25eafe5b144588be353a9637
terminological-inexactitude <georgeboy389@gmail.com>
2024-07-03 13:45:39 +0200

    Update tictactoe.cpp

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -11,7 +11,7 @@ void place_mark(int row, int col, char player, char board[3][3]);
 void clear_board();
 void write_var_new(const char array[]);
 
-bool gBoardFull = FALSE;
+bool gBoardFull = false;
 
 int main ()
 {
