b48723e356541c3b6af010946ecb53c93da31793
terminological-inexactitude <georgeboy389@gmail.com>
2024-07-23 15:19:40 +0200

    Update connect4.cpp

--- connect4.cpp
+++ connect4.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <fstream>
+
+#define getvar "{getchannel:game}"
+
+void writeVar(const char array[]){
+    std::ofstream file("./output/__internals__.json");
+    file << R"({"storage":{"server":{},"channel":{"game":")" << array << R"("},"channel":{}}})";
+}
 
 int main() {
     printf("asf");
-
+	
+	writeVar("gamestate example");
+	
     return 0;
 }
