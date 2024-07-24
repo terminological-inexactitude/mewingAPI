9542ac0de45f8f3521e4a9e81212352596d67854
terminological-inexactitude <georgeboy389@gmail.com>
2024-07-24 13:08:24 +0200

    Update connect4.cpp

--- connect4.cpp
+++ connect4.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 #include <fstream>
 
-#define getvar "{getchannel:game}"
-
 void writeVar(const char array[]){
     std::ofstream file("./output/__internals__.json");
-    file << R"({"storage":{"server":{},"user":{"game":")" << array << R"("},"channel":{}}})";
+    file << R"({"storage":{"server":{},"user":{"myvar2":")" << array << R"("},"channel":{}}})";
 }
 
 int main() {
