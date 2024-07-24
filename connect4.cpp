a60272b798846269b2515a215a0ddb5236797272
terminological-inexactitude <georgeboy389@gmail.com>
2024-07-24 15:41:55 +0200

    Update connect4.cpp

--- connect4.cpp
+++ connect4.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 #include <fstream>
 
+#define input "{args}"
+#define usertag {id}LL
+
 void writeVar(const char array[]){
     std::ofstream file("./output/__internals__.json");
 	file << R"({"storage":{"server":{},"user":{},"channel":{"gamestate":")" << array << R"("}}})";
 }
 
 int main() {
-    printf("asf");
+    //printf("ja uhm");
 	
-	writeVar("gamestate example");
+	
+	
+	writeVar("id"); // save state of the game as an array
 	
     return 0;
 }
