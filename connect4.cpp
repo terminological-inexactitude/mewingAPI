47fe11582224dab042a9281c0829e08bdaef3abe
terminological-inexactitude <georgeboy389@gmail.com>
2024-07-24 13:18:49 +0200

    Update connect4.cpp

--- connect4.cpp
+++ connect4.cpp
@@ -3,7 +3,7 @@
 
 void writeVar(const char array[]){
     std::ofstream file("./output/__internals__.json");
-    file << R"({"storage":{"server":{},"user":{"myvar2":")" << array << R"("},"channel":{}}})";
+	file << R"({"storage":{"server":{},"user":{},"channel":{"gamestate":")" << array << R"("}}})";
 }
 
 int main() {
