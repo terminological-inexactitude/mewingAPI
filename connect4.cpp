e27bd4c722369908c25c1bec9cc69f7c00319d82
terminological-inexactitude <georgeboy389@gmail.com>
2024-07-24 16:09:20 +0200

    Update connect4.cpp

--- connect4.cpp
+++ connect4.cpp
@@ -4,15 +4,39 @@
 #define input "{args}"
 #define usertag {id}LL
 
+#define ROWS 6
+#define COLS 7
+#define EMPTY '.'
+
+char gboard[6][7]; // row, column
+
 void writeVar(const char array[]){
     std::ofstream file("./output/__internals__.json");
 	file << R"({"storage":{"server":{},"user":{},"channel":{"gamestate":")" << array << R"("}}})";
 }
+void clearArray() { writeVar(".........................................."); }
+
+void printBoard() {
+    for (int r = 0; r < 6; ++r) { // rows
+        for (int c = 0; c < 7; ++c) { // columns
+            printf("%c ", gboard[r][c]);
+        }
+        printf("\n");
+    }
+}
+
+int dropCoin(int col, char coin) {
+    for (int r = ROWS - 1; r >= 0; --r) {
+        if (gboard[r][col] == EMPTY) {
+            gboard[r][col] = coin;
+            return 1;
+        }
+    }
+    return 0;
+}
 
 int main() {
-    //printf("ja uhm");
-	
-	
+    //printf("test");
 	
 	writeVar("id"); // save state of the game as an array
 	
