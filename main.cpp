cb4b5849c836e834866813e5c843230aaa1540bd
terminological-inexactitude <georgeboy389@gmail.com>
2024-07-26 13:51:52 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,11 +3,12 @@
 #define usertag {id}LL
 
 int main() {
-    printf("test");
-    printf("\n");
     printf("# <:kleiner_happy:363444088965431298>");
+    printf("\n\n\n");
     
-    //printf("\n\n");
+    printf("Your emoji of today is:\n#");
+    printf("<:kleiner_happy:363444088965431298>");
+    
     //printf("%lld", usertag);
     
     return 0;
