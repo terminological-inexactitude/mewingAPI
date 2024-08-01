8ce673f3bcefb3a7d5a49e1136fe90d233b7ae85
terminological-inexactitude <georgeboy389@gmail.com>
2024-08-01 11:03:36 +0200

    Update emojihoroscope.cpp

--- emojihoroscope.cpp
+++ emojihoroscope.cpp
@@ -11,10 +11,9 @@ int main()
     tzset(); // apply timezone change
 	
     int daysSinceEpoch = seconds / (60 * 60 * 24);
-
     unsigned long long randomseed = usertag + daysSinceEpoch; // ensure user's only get 1 a day
+    
     srand(randomseed);
-
     int p = rand() % 35 + 1; // Range of 1 to 35 
 
     const char* s;
