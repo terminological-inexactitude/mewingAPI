63b048de398525951a805e77b11dcf21a704c749
terminological-inexactitude <georgeboy389@gmail.com>
2024-08-01 10:22:46 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,19 @@
 #include <cstdio>
-
+#include <cstdlib>
+#include <ctime>
 #define usertag {id}LL
 
-int main() {  
-    //printf("Your emoji of today is:\n# <:kleiner_happy:363444088965431298>");
-
-    printf("# <:kleiner_happy:363444088965431298>\n\n");
-
-    printf("# <:kleiner_happy:363444088965431298><:kleiner_happy:363444088965431298><:kleiner_happy:363444088965431298>\n");
-    printf("# <:kleiner_happy:363444088965431298><:kleiner_happy:363444088965431298><:kleiner_happy:363444088965431298>\n");
-    printf("# <:kleiner_happy:363444088965431298><:kleiner_happy:363444088965431298><:kleiner_happy:363444088965431298>\n");
+int main() 
+{
+    time_t seconds;
     
-    printf("\n");
-    printf("<:skibid:1207635444909481984>\n");
-    printf("# <:skibid:1207635444909481984>");
+    seconds = time(NULL);
+    int daysSinceEpoch = seconds / (60 * 60 * 24);
 
-    
-    //printf("%lld", usertag);
-    
-    return 0;
+    unsigned long long randomseed = usertag + daysSinceEpoch; // ensure user's only get 1 a day
+    srand(randomseed);
+
+    int p = rand() % 35 + 1; // Range of 1 to 35 
+	
+	printf("Number: %d", p);
 }
