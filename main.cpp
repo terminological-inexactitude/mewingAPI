e89e6d76f43dd6a90e3a9a010265f721692fa8ff
terminological-inexactitude <georgeboy389@gmail.com>
2024-08-01 10:43:05 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,15 +5,18 @@
 
 int main() 
 {
-    time_t seconds;
-    
-    seconds = time(NULL);
+    time_t seconds = time(NULL);	
+	
     int daysSinceEpoch = seconds / (60 * 60 * 24);
-
     unsigned long long randomseed = usertag + daysSinceEpoch; // ensure user's only get 1 a day
-    srand(randomseed);
-
+    
+	srand(randomseed);
     int p = rand() % 35 + 1; // Range of 1 to 35 
+	
+	
+	
+	char* dt = ctime(&seconds);  // Convert time_t to a human-readable string
+	printf("The local date and time is: %s", dt);  // Print the string
 	
 	printf("Number: %d", p);
 }
