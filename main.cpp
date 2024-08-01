d04c88130cca592bcf56b6719e689e78920ccb3d
terminological-inexactitude <georgeboy389@gmail.com>
2024-08-01 10:47:36 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@ int main()
 {
     time_t seconds = time(NULL);	
 	
+	setenv("TZ", "Europe/Amsterdam", 1); // Set the timezone environment variable to Amsterdam
+    tzset(); // Apply the timezone change
+	
     int daysSinceEpoch = seconds / (60 * 60 * 24);
     unsigned long long randomseed = usertag + daysSinceEpoch; // ensure user's only get 1 a day
     
@@ -14,9 +17,8 @@ int main()
     int p = rand() % 35 + 1; // Range of 1 to 35 
 	
 	
-	
 	char* dt = ctime(&seconds);  // Convert time_t to a human-readable string
-	printf("The local date and time is: %s", dt);  // Print the string
+	printf("The updated local date and time is: %s", dt);  // Print the string
 	
 	printf("Number: %d", p);
 }
