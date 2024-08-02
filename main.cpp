42e9cdadbbceb1f5d0fe7a576b3f9849367437fc
terminological-inexactitude <georgeboy389@gmail.com>
2024-08-02 16:15:21 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,25 +1,22 @@
-#include <cstdio>	 // printf
-#include <ctime>     // time
-#include <cstdlib>   // setenv and tzset
+#include <cstdio>
 
 #define usertag {id}LL
 
-int main() 
-{
-    time_t seconds = time(NULL);	
-	
-	setenv("TZ", "Europe/Amsterdam", 1); // Set the timezone environment variable to Amsterdam
-    tzset(); // Apply the timezone change
-	
-    int daysSinceEpoch = seconds / (60 * 60 * 24);
-    unsigned long long randomseed = usertag + daysSinceEpoch; // ensure user's only get 1 a day
+int main() {  
+    //printf("Your emoji of today is:\n# <:kleiner_happy:363444088965431298>");
+
+    printf("# <:kleiner_happy:363444088965431298>\n\n");
+
+    printf("# <:kleiner_happy:363444088965431298><:kleiner_happy:363444088965431298><:kleiner_happy:363444088965431298>\n");
+    printf("# <:kleiner_happy:363444088965431298><:kleiner_happy:363444088965431298><:kleiner_happy:363444088965431298>\n");
+    printf("# <:kleiner_happy:363444088965431298><:kleiner_happy:363444088965431298><:kleiner_happy:363444088965431298>\n");
     
-	srand(randomseed);
-    int p = rand() % 35 + 1; // Range of 1 to 35 
-	
-	
-	char* dt = ctime(&seconds);  // Convert time_t to a human-readable string
-	printf("The updated local date and time is: %s", dt);  // Print the string
-	
-	printf("Number: %d", p);
+    printf("\n");
+    printf("<:skibid:1207635444909481984>\n");
+    printf("# <:skibid:1207635444909481984>");
+
+    
+    //printf("%lld", usertag);
+    
+    return 0;
 }
