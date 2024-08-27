dfc2319088316b97e960feb70427b1f5d60b3c4d
terminological-inexactitude <georgeboy389@gmail.com>
2024-08-27 13:59:25 +0200

    Update tictactoe.cpp

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -17,7 +17,7 @@ void clearArray() { writeVar("EEEEEEEEE"); }
 void printBoard() {
     const char* nums[] = {"", "1️⃣", "2️⃣", "3️⃣"};
     
-    printf("\n```\n  %s %s %s\n", nums[1], nums[2], nums[3]);
+    printf("\n```\n   %s  %s  %s\n", nums[1], nums[2], nums[3]);
     
     for (int i = 0; i < 3; ++i) {
         printf("%s %c | %c | %c\n", nums[i + 1], gBoard[i][0], gBoard[i][1], gBoard[i][2]);
